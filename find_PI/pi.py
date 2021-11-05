3b2d1f591590e5ee9e731b35804047f6c13fa880
IvanYatsuk <85255504+IvanYatsuk@users.noreply.github.com>
2021-11-05 16:28:56 +0300

    Update pi.py

--- find_PI/pi.py
+++ find_PI/pi.py
@@ -1,1 +1,9 @@
-
+iterations = int(input())
+denomimator = float(1.0)
+multiplier = float(1.0)
+pi = 4 / denomimator
+for i in range(iterations):
+    denomimator += 2.0
+    multiplier *= -1.0
+    pi += ( (4.0 / denomimator) * multiplier )
+    print(pi)
